d3d27933fedc6683ee7b93161e48c4eca4010876
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-14 03:15:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,26 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+using_first = ["wow", "banana", "cat", "dog"]
+using_first.first
 end
 
 def using_last(array)
-
+using_last = ["apple", "banana", "cat", "arrays!"]
+using_last.last
 end
 
 def using_size(array)
-
+using_size = ["apple", "banana", "cat", "dog", "egg", "frog"]
+using_size.size
 end
